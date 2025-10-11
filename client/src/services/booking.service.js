b7a071f21d1f84b8e92f94e697e2124aea5a95de
PALALA\bthin <bthinh003@gmail.com>
2025-10-11 16:35:47 +0700

    fix: service

--- client/src/services/booking.service.js
+++ client/src/services/booking.service.js
@@ -2,69 +2,75 @@
 import api from "../lib/axios";
 
 const BookingService = {
+  // ======================= MENTEE =======================
+
   createBooking: async (data) => {
-    const response = await api.post("/api/bookings", data);
-    return response.data;
-  },
-
-  getBookedSlots: async (mentorId) => {
-    const response = await api.get(`/api/bookings/mentor/${mentorId}/booked-slots`);
-    return response.data;
-  },
-
-  cancelBooking: async (bookingId) => {
-    const response = await api.patch(`/api/bookings/${bookingId}/cancel`);
-    return response.data;
-  },
-
-  getMenteeBookings: async () => {
-    const response = await api.get(`/api/bookings/mentee`);
-    return response.data;
-  },
-
-  getMenteeBookedSlots: async () => {
-    const response = await api.get(`/api/bookings/mentor/applications`);
-    return response.data;
-  },
-
-  getBookingStatusByMenteeId: async (params = {}) => {
-    const response = await api.get(`/api/bookings/mentee/status`, { params });
-    return response.data;
-  },
-
-  getLearningProgress: async (params = {}) => {
-    const response = await api.get(`/api/bookings/mentee/progress`, { params });
-    return response.data;
+    const res = await api.post("/api/bookings", data);
+    return res.data;
   },
 
   recreatePaymentLink: async (bookingId) => {
-    const response = await api.put(`/api/bookings/${bookingId}/recreate-payment`);
-    return response.data;
+    const res = await api.post(`/api/bookings/${bookingId}/recreate-payment`);
+    return res.data;
   },
+
+  cancelBooking: async (bookingId) => {
+    const res = await api.patch(`/api/bookings/${bookingId}/cancel`);
+    return res.data;
+  },
+
+  getMenteeBookings: async () => {
+    const res = await api.get("/api/bookings/mentee");
+    return res.data;
+  },
+
+  getBookingStatusByMenteeId: async (params = {}) => {
+    const res = await api.get("/api/bookings/mentee/status", { params });
+    return res.data;
+  },
+
+  getLearningProgress: async (params = {}) => {
+    const res = await api.get("/api/bookings/mentee/progress", { params });
+    return res.data;
+  },
+
+  // ======================= MENTOR =======================
 
   getMentorApplications: async () => {
-    const response = await api.get(`/api/bookings/applications`);
-    return response.data;
-  },
-
-  getApplicationDetails: async (bookingId) => {
-    const response = await api.get(`/api/bookings/applications/${bookingId}`);
-    return response.data;
+    const res = await api.get("/api/bookings/applications");
+    return res.data;
   },
 
   updateApplicationStatus: async (data) => {
-    const response = await api.patch(`/api/bookings/applications/action/status`, data);
-    return response.data;
+    const res = await api.patch("/api/bookings/applications/action/status", data);
+    return res.data;
   },
 
-  updateSessionByMentor: async (data) => {
-    const response = await api.patch(`/api/bookings/applications/session`, data);
-    return response.data;
+  updateSessionByMentor: async (applicationId, data) => {
+    const res = await api.patch(`/api/bookings/applications/${applicationId}/session`, data);
+    return res.data;
+  },
+
+  getApplicationDetails: async (applicationId) => {
+    const res = await api.get(`/api/bookings/applications/${applicationId}`);
+    return res.data;
+  },
+
+  getMentorBookedSlots: async () => {
+    const res = await api.get("/api/bookings/mentor/applications");
+    return res.data;
   },
 
   getTeachProgress: async () => {
-    const response = await api.get('/api/bookings/mentor/learning/progress');
-    return response.data;
+    const res = await api.get("/api/bookings/mentor/learning/progress");
+    return res.data;
+  },
+
+  // ======================= PUBLIC =======================
+
+  getBookedSlots: async (mentorId) => {
+    const res = await api.get(`/api/bookings/mentor/${mentorId}/booked-slots`);
+    return res.data;
   },
 };
 
